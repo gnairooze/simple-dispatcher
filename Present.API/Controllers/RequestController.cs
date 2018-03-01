c3449d8b8d5f39a3eab48aa8206a5f20c8e80a36
gnairooze <gnairooze@gmail.com>
2018-03-01 17:49:09 +0200

    fix log implementation

--- Present.API/Controllers/RequestController.cs
+++ Present.API/Controllers/RequestController.cs
@@ -1,10 +1,12 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 
@@ -40,85 +42,142 @@ namespace SimpleDispatcher.Present.API.Controllers
 
             HttpResponseMessage result = new HttpResponseMessage();
 
-            #region basic validadtion
-            Mora.Common.Method.Parameter.Result<List<string>> basicValidationResult = Validation.ValidateAdd(data);
-            //API_Business.Logger.Log()
-            if (basicValidationResult.Code != Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
+            try
             {
-                result.StatusCode = HttpStatusCode.BadRequest;
-                string validationData = JsonConvert.SerializeObject(basicValidationResult);
-                result.Content = new StringContent(validationData);
+                #region basic validadtion
+                Mora.Common.Method.Parameter.Result<List<string>> basicValidationResult = Validation.ValidateAdd(data);
+                //API_Business.Logger.Log()
+                if (basicValidationResult.Code != Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
+                {
+                    result.StatusCode = HttpStatusCode.BadRequest;
+                    string validationData = JsonConvert.SerializeObject(basicValidationResult);
+                    result.Content = new StringContent(validationData);
+
+                    #region log
+                    logModel.BusinessID = Guid.NewGuid();
+                    logModel.Counter = logCounter++;
+                    logModel.CreatedOn = DateTime.Now;
+                    logModel.When = DateTime.Now;
+                    logModel.LogType = ILogger.TypeOfLog.Warning;
+                    logModel.What = "Failed basic validation. data: " + inputData + ". validation: " + validationData;
+
+                    API_Business.Logger.Log(logModel);
+                    #endregion
+
+                    return result;
+                }
+                #endregion
+
+                #region add request
+                Business.View.Request.AddView modelAddRequest = new Business.View.Request.AddView()
+                {
+                    BusinessID = Guid.Parse(data["businessID"].ToString()),
+                    Data = data.ToString(),
+                    Operation = data["operation"].ToString(),
+                    ReferenceName = data["referenceName"].ToString(),
+                    ReferenceValue = data["referenceValue"].ToString()
+                };
+
+                Mora.Common.Method.Parameter.Result<List<string>> addRequestResult = API_Business.Manager.AddRequest(new Business.View.Request.AddView()
+                {
+                    BusinessID = Guid.Parse(data["businessID"].ToString()),
+                    Data = data.ToString(),
+                    Operation = data["operation"].ToString(),
+                    ReferenceName = data["referenceName"].ToString(),
+                    ReferenceValue = data["referenceValue"].ToString()
+                });
+
+                string logAddRequestResult = JsonConvert.SerializeObject(addRequestResult);
+                string logAddRequestView = JsonConvert.SerializeObject(modelAddRequest);
+
+                if (addRequestResult.Code == Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
+                {
+                    result.StatusCode = HttpStatusCode.Accepted;
+                }
+                else
+                {
+                    result.StatusCode = HttpStatusCode.BadRequest;
+
+                    result.Content = new StringContent(logAddRequestResult);
+                }
+                #endregion
 
                 #region log
                 logModel.BusinessID = Guid.NewGuid();
                 logModel.Counter = logCounter++;
                 logModel.CreatedOn = DateTime.Now;
                 logModel.When = DateTime.Now;
-                logModel.LogType = ILogger.TypeOfLog.Warning;
-                logModel.What = "Failed basic validation. data: " + inputData + ". validation: " + validationData;
+
+                if (addRequestResult.Code == Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
+                {
+                    logModel.LogType = ILogger.TypeOfLog.Info;
+                    logModel.What = "Request added successfully. data: " + inputData + ". addRequestView: " + logAddRequestView;
+                }
+                else
+                {
+                    logModel.LogType = ILogger.TypeOfLog.Warning;
+                    logModel.What = "Failed adding request. data: " + inputData + ". addRequestView: " + logAddRequestView + ". addRequestResult: " + logAddRequestResult;
+                }
 
                 API_Business.Logger.Log(logModel);
                 #endregion
-
-                return result;
             }
-            #endregion
-
-            #region add request
-            Business.View.Request.AddView modelAddRequest = new Business.View.Request.AddView()
+            catch (Exception ex)
             {
-                BusinessID = Guid.Parse(data["businessID"].ToString()),
-                Data = data.ToString(),
-                Operation = data["operation"].ToString(),
-                ReferenceName = data["referenceName"].ToString(),
-                ReferenceValue = data["referenceValue"].ToString()
-            };
+                var id = Guid.NewGuid();
+                StringBuilder bld = new StringBuilder();
 
-            Mora.Common.Method.Parameter.Result<List<string>> addRequestResult = API_Business.Manager.AddRequest(new Business.View.Request.AddView()
-            {
-                BusinessID = Guid.Parse(data["businessID"].ToString()),
-                Data = data.ToString(),
-                Operation = data["operation"].ToString(),
-                ReferenceName = data["referenceName"].ToString(),
-                ReferenceValue = data["referenceValue"].ToString()
-            });
+                logModel.LogType = ILogger.TypeOfLog.Error;
+                logModel.What += ". Error: " + extractError(ex, ref bld);
+                logModel.BusinessID = Guid.NewGuid();
+                logModel.ReferenceName = "Tool";
+                logModel.ReferenceValue = "Present.API";
 
-            string logAddRequestResult = JsonConvert.SerializeObject(addRequestResult);
-            string logAddRequestView = JsonConvert.SerializeObject(modelAddRequest);
-
-            if (addRequestResult.Code == Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
-            {
-                result.StatusCode = HttpStatusCode.Accepted;
+                API_Business.Logger.Log(logModel);
             }
-            else
-            {
-                result.StatusCode = HttpStatusCode.BadRequest;
-
-                result.Content = new StringContent(logAddRequestResult);
-            }
-            #endregion
-
-            #region log
-            logModel.BusinessID = Guid.NewGuid();
-            logModel.Counter = logCounter++;
-            logModel.CreatedOn = DateTime.Now;
-            logModel.When = DateTime.Now;
             
-            if(addRequestResult.Code == Mora.Common.Vault.Result.RESULT_SUCCEEDED_CODE)
-            {
-                logModel.LogType = ILogger.TypeOfLog.Info;
-                logModel.What = "Request added successfully. data: " + inputData + ". addRequestView: " + logAddRequestView;
-            }
-            else
-            {
-                logModel.LogType = ILogger.TypeOfLog.Warning;
-                logModel.What = "Failed adding request. data: " + inputData + ". addRequestView: " + logAddRequestView + ". addRequestResult: " + logAddRequestResult;
-            }
-
-            API_Business.Logger.Log(logModel);
-            #endregion
-
             return result;
+        }
+
+        private string extractError(Exception ex, ref StringBuilder bld)
+        {
+            if (ex.Data != null && ex.Data.Count > 0)
+            {
+                foreach (DictionaryEntry item in ex.Data)
+                {
+                    bld.AppendLine(String.Format("Data Key:{0}. Data Value:{1}.", item.Key, item.Value));
+                }
+            }
+
+            bld.AppendLine(String.Format("Message:{0}.", ex.Message));
+            bld.AppendLine(String.Format("StackTrace:{0}.", ex.StackTrace));
+
+            if (ex.InnerException != null)
+            {
+                extractError(ex.InnerException, ref bld);
+            }
+
+            return bld.ToString();
+        }
+
+        private string getWho(string clientIP)
+        {
+            string name = System.Net.Dns.GetHostName();
+            System.Net.IPAddress[] ips = System.Net.Dns.GetHostAddresses(name);
+
+            List<string> allIPs = new List<string>();
+
+            if (ips != null)
+            {
+                foreach (System.Net.IPAddress ip in ips)
+                {
+                    allIPs.Add(ip.ToString());
+                }
+            }
+
+            string who = string.Format("Client IP:{0} | HostName:{1} | IPs:{2} | Location:{3} | Assembly:{4} | Class:{5} | Method:{6}", clientIP, name, string.Join(",", allIPs.ToArray()), System.Reflection.Assembly.GetExecutingAssembly().Location, System.Reflection.Assembly.GetExecutingAssembly().FullName, this.GetType(), "Post");
+
+            return who;
         }
     }
 }
--- Present.QueueProcessor/Program.cs
+++ Present.QueueProcessor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,86 @@ namespace SimpleDispatcher.Present.QueueProcessor
     {
         static void Main(string[] args)
         {
-            Business.Process.Queue queue = new Business.Process.Queue(Present.QueueProcessor.Properties.Settings.Default.Module,string.Empty, QueueProcessor.Properties.Settings.Default.QueueID, 
-                QueueProcessor.Properties.Settings.Default.TopCount, 
-                new DBLogger.SimpleDbLogger() {
-                    CanAddError = Present.QueueProcessor.Properties.Settings.Default.CanAddError,
-                    CanAddInfo = Present.QueueProcessor.Properties.Settings.Default.CanAddInfo,
-                    CanAddWarning = Present.QueueProcessor.Properties.Settings.Default.CanAddWarning
-                },
-                Business.Process.Vault.ExecType.ApiWorker);
+            DBLogger.SimpleDbLogger logger = new DBLogger.SimpleDbLogger()
+            {
+                CanAddError = Present.QueueProcessor.Properties.Settings.Default.CanAddError,
+                CanAddInfo = Present.QueueProcessor.Properties.Settings.Default.CanAddInfo,
+                CanAddWarning = Present.QueueProcessor.Properties.Settings.Default.CanAddWarning
+            };
 
-            queue.Run(string.Empty);
-            //queue.RunAsync(string.Empty);
+            try
+            {
+                Business.Process.Queue queue = new Business.Process.Queue(Present.QueueProcessor.Properties.Settings.Default.Module, string.Empty, QueueProcessor.Properties.Settings.Default.QueueID, QueueProcessor.Properties.Settings.Default.TopCount, logger, Business.Process.Vault.ExecType.ApiWorker);
+
+                queue.Run(string.Empty);
+                //queue.RunAsync(string.Empty);
+            }
+            catch (Exception ex)
+            {
+                var id = Guid.NewGuid();
+                StringBuilder bld = new StringBuilder();
+
+                logger.Log(new ILogger.LogModel()
+                {
+                    BusinessID = id,
+                    Counter = -1,
+                    CreatedOn = DateTime.Now,
+                    Group = id,
+                    LogType = ILogger.TypeOfLog.Error,
+                    Module = Present.QueueProcessor.Properties.Settings.Default.Module,
+                    ReferenceName = "Tool",
+                    ReferenceValue = Present.QueueProcessor.Properties.Settings.Default.Module,
+                    When = DateTime.Now,
+                    Who = getWho(),
+                    What = extractError(ex, ref bld)
+                });
+            }
+            
 
             Console.WriteLine("Press any key to exit ...");
             Console.Read();
         }
+
+        private static string extractError(Exception ex, ref StringBuilder bld)
+        {
+            if (ex.Data != null && ex.Data.Count > 0)
+            {
+                foreach (DictionaryEntry item in ex.Data)
+                {
+                    bld.AppendLine(String.Format("Data Key:{0}. Data Value:{1}.", item.Key, item.Value));
+                }
+            }
+
+            bld.AppendLine(String.Format("Message:{0}.", ex.Message));
+            bld.AppendLine(String.Format("StackTrace:{0}.", ex.StackTrace));
+
+            if (ex.InnerException != null)
+            {
+                extractError(ex.InnerException, ref bld);
+            }
+
+            return bld.ToString();
+        }
+
+        private static string getWho()
+        {
+            string name = System.Net.Dns.GetHostName();
+            System.Net.IPAddress[] ips = System.Net.Dns.GetHostAddresses(name);
+
+            List<string> allIPs = new List<string>();
+
+            if (ips != null)
+            {
+                foreach (System.Net.IPAddress ip in ips)
+                {
+                    allIPs.Add(ip.ToString());
+                }
+            }
+
+            string who = string.Format("HostName:{0} | IPs:{1} | Location:{2} | Assembly:{3} | Class:{4} | Method:{5}", name, string.Join(",", allIPs.ToArray()), System.Reflection.Assembly.GetExecutingAssembly().Location, System.Reflection.Assembly.GetExecutingAssembly().FullName, "Present.QueueProcessor.Program", "Main");
+
+            return who;
+        }
+
     }
 }
